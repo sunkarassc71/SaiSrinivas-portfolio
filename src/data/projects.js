2426992197cc78d821ec9464bbf4be6541764f18
sunkarassc71 <120734051+sunkarassc71@users.noreply.github.com>
2025-07-11 16:35:37 +0530

    modified project file

---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+const projects = [
+  {
+    projectName: "Employee Portal",
+    description: "A secure and integrated internal platform designed to efficiently manage employee records, leave applications, project tracking, expense reporting, billing, asset management, service desk operations, and organizational announcements.",
+    company: "System Soft Technologies",
+    image: require('../assets/Project1.png'),
+    frontend: "Angular, BootStrap, CSS",
+    backend: "Java, Spring Boot, Hibernate",
+    database: 'MySQL, MongoDB',
+    link: null
+  },
+  {
+    projectName: "Portfolio Site",
+    description: "A responsive digital portfolio highlighting my projects, skills, education, and experience — designed to showcase my journey and make it easy to connect with site visitors.",
+    company: "Own Project",
+    image: require('../assets/Project2.png'),
+    frontend: "React, Tailwind CSS",
+    backend: "Node.js with Express",
+    database: 'MongoDB (via Mongoose ODM)',
+    link: "https://github.com/sunkarassc71/SaiSrinivas-portfolio"
+  }
+];
+
+export default projects;
