aa43f82948fe1dfbf1080fb7688ae7c323a40a76
sunkarassc71 <120734051+sunkarassc71@users.noreply.github.com>
2025-07-11 17:35:58 +0530

    Implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import Header from './components/Header';
 import Hero from './components/Hero';
@@ -14,13 +15,22 @@ function App() {
   return (
     <div className="bg-gray-50 text-gray-900">
       <Header />
-      <Hero />
-      <AboutMe />
-      <Projects />
-      <Experience />
-      <Education />
-      <Skills />
-      <CustomerDetails />
+      <Routes>
+        <Route path="/" element={<>
+          <Hero />
+          {/* <AboutMe />
+          <Projects />
+          <Experience /> */}
+          <Education />
+          <Skills />
+          <CustomerDetails />
+        </>} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
       <Footer />
     </div>
   )
